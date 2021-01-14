961963f2c277fb89966de7f60cbe9a9d583d66ab
mbround18 <12646562+mbround18@users.noreply.github.com>
2021-01-13 19:36:56 -0800

    graphql request info

--- utils/graphql-client.ts
+++ utils/graphql-client.ts
@@ -4,12 +4,16 @@ import {GraphQLClient} from 'graphql-request';
 
 export class GraphQL {
   public static client(): GraphQLClient {
+
     const authorization = localStorage.getItem('auth._token.discord');
     if (!authorization)
       throw new Error('Authorization not found! Please log in!');
     return new GraphQLClient(process.env.ENDPOINT_URL ?? '', {
-      mode: "no-cors",
-      headers: {authorization}
+      mode: 'no-cors',
+      headers: {
+        authorization,
+        "Access-Control-Request-Headers": "*"
+      }
     });
   }
 }
