94768efd5b2430903a6f4d5260835955119cf9b5
mbround18 <12646562+mbround18@users.noreply.github.com>
2021-04-16 21:45:21 -0700

    fixed eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    semi: 2,
+    "no-useless-constructor": "off",
+    quotes: ["error", "double"]
+  }
+}
